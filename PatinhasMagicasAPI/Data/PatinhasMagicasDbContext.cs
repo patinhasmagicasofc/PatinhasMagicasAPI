f947388120892a853eead560b08e9b7e38d93d9f
jtn-san <santosjonnathan@aol.com>
2025-09-26 21:18:19 -0300

    Correção Universal

--- PatinhasMagicasAPI/Data/PatinhasMagicasDbContext.cs
+++ PatinhasMagicasAPI/Data/PatinhasMagicasDbContext.cs
@@ -5,7 +5,19 @@ namespace PatinhasMagicasAPI.Data
 {
     public class PatinhasMagicasDbContext : DbContext
     {
-        public PatinhasMagicasDbContext(DbContextOptions<PatinhasMagicasDbContext> options) : base(options) { }
+        public PatinhasMagicasDbContext(DbContextOptions<PatinhasMagicasDbContext> options) : base(options) {
+        
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<TipoUsuario> TiposUsuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Agendamento> Agendamentos { get; set; }
+        public DbSet<StatusAgendamento> StatusAgendamentos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedido { get; set; }
 
     }
 
