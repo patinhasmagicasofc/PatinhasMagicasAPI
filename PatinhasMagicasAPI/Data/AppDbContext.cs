cbe84db7185f7c9fba299673bfee82e561b45305
luan.mpereira2 <luan.mpereira2@SMP0711457W11-1>
2025-09-29 21:39:02 -0300

    subindo Dbcontext

---
+++ PatinhasMagicasAPI/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatinhasMagicasAPI.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace PatinhasMagicasAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Servico> Servicos { get; set; }
+        public DbSet<TipoServico> TiposServico { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TipoServico>()
+                .HasKey(t => t.IdTipoServico);
+
+            modelBuilder.Entity<Servico>()
+                .HasKey(s => s.IdServico);
+
+            modelBuilder.Entity<Servico>()
+                .HasOne(s => s.TipoServico)
+                .WithMany(t => t.Servicos)
+                .HasForeignKey(s => s.IdTipoServico);
+        }
+    }
+}
